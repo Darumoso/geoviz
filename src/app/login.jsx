6be60371fa2d987b7d643da617ca93da2a44e289
LEGM2002 <luiseduardogarciamartinez92@gmail.com>
2024-12-09 00:08:05 -0600

    Simple users dashboard

--- src/app/login.jsx
+++ src/app/login.jsx
@@ -15,30 +15,30 @@ export default function Login() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-900">
+        <div className='min-h-screen flex items-center justify-center bg-blue-900'>
             {/* Page name and logo */}
-            <div className="absolute top-10 left-12 text-white text-6xl font-bold flex items-center">
-                <img src="/LogoGeoviz.png" alt="Logo" className="w-32 h-32 mr-4 object-contain"/>
+            <div className='absolute top-10 left-12 text-white text-6xl font-bold flex items-center'>
+                <img src='/LogoGeoviz.png" alt="Logo" className="w-32 h-32 mr-4 object-contain'/>
                 GEOVIZ
             </div>
-            {/* Logic form */}
-            <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-center text-2xl font-bold mb-4">BIENVENIDO</h2>
-                <div className="mb-4">
-                    <label className="block text-gray-700 p-4">Usuario</label>
+            {/* Login form */}
+            <div className='bg-white p-10 rounded-lg shadow-lg w-full max-w-md'>
+                <h2 className='text-center text-2xl font-bold mb-4'>BIENVENIDO</h2>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 p-4'>Usuario</label>
                     <Input 
                         type='text'
                     />
                 </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 p-4">Contraseña</label>
-                    <div className="relative">
+                <div className='mb-4'>
+                    <label className='block text-gray-700 p-4'>Contraseña</label>
+                    <div className='relative'>
                         <Input 
                             type='password'
                         />
                     </div>
                 </div>
-                <div className="p-4 mt-auto flex justify-center">
+                <div className='p-4 mt-auto flex justify-center'>
                     <Button 
                         children='Ingresar'
                     />  
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
 import Login from "./login"; 
+import UsersDashboard from "./usersDashboard";
 
 export default function Home() {
   return (
-    <Login />
+    <UsersDashboard />
   );
 }
