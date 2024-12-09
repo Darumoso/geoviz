34e8849c0741b8f1bc6c0682bd416ac4d474297d
Daruma <hectoryoramquiroz@gmail.com>
2024-12-08 20:07:36 -0600

    Refactor: quitando fuentes personalizadas

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,4 @@
-import localFont from "next/font/local";
 import "./ui/globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "GeoViz",
@@ -21,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
